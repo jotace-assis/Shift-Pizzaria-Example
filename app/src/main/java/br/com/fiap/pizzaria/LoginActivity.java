3948b49845ecdee0449df6afd3fc588c136823e4
jotaceassis1 <ec0nom1cs>
2017-09-30 15:34:15 -0300

    parcial 1

--- app/src/main/java/br/com/fiap/pizzaria/LoginActivity.java
+++ app/src/main/java/br/com/fiap/pizzaria/LoginActivity.java
@@ -4,11 +4,14 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextInputLayout inputLogin;
     private TextInputLayout inputPassword;
+    private TextView tvMessage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +20,29 @@ public class LoginActivity extends AppCompatActivity {
 
         inputLogin = (TextInputLayout) findViewById(R.id.inputLogin);
         inputPassword = (TextInputLayout) findViewById(R.id.inputPassword);
+        tvMessage = (TextView) findViewById(R.id.outputMessage);
     }
 
     public void conectar(View v) {
         String login = inputLogin.getEditText().getText().toString();
         String password = inputPassword.getEditText().getText().toString();
+
+        tvMessage.setVisibility(View.GONE);
+
+        inputLogin.setError(getString(R.string.message_campo_obrigatorio));
+        inputPassword.setError(getString(R.string.message_campo_obrigatorio));
+
+        inputLogin.setErrorEnabled(login.trim().equals(""));
+        inputPassword.setErrorEnabled(password.trim().equals(""));
+
+        if (login.equals("android") && password.equals("123")) {
+            // SUCESSO
+        }
+        else {
+            // FALHA
+            tvMessage.setVisibility(View.VISIBLE);
+            tvMessage.setText("Usuário ou senha inválidos");
+        }
+
     }
 }
