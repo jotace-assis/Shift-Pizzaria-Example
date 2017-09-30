f80c4d1755f18efd27ca59e231b7f49803c37975
jotaceassis1 <ec0nom1cs>
2017-09-30 14:51:46 -0300

    parcial 1

--- app/src/main/java/br/com/fiap/pizzaria/LoginActivity.java
+++ app/src/main/java/br/com/fiap/pizzaria/LoginActivity.java
@@ -1,13 +1,26 @@
 package br.com.fiap.pizzaria;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputLayout inputLogin;
+    private TextInputLayout inputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        inputLogin = (TextInputLayout) findViewById(R.id.inputLogin);
+        inputPassword = (TextInputLayout) findViewById(R.id.inputPassword);
+    }
+
+    public void conectar(View v) {
+        String login = inputLogin.getEditText().getText().toString();
+        String password = inputPassword.getEditText().getText().toString();
     }
 }
