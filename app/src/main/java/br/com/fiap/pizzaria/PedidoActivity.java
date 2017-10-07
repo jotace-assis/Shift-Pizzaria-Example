8eb69ba1bbc2b1b1c93ec2ab99e43fb58e341ad7
jotace.assis <jotace.assis@outlook.com>
2017-10-07 10:59:43 -0300

    Preparacao classe pedido com butterKnife

--- app/src/main/java/br/com/fiap/pizzaria/PedidoActivity.java
+++ app/src/main/java/br/com/fiap/pizzaria/PedidoActivity.java
@@ -2,12 +2,41 @@ package br.com.fiap.pizzaria;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class PedidoActivity extends AppCompatActivity {
+
+    @BindView(R.id.cbAtum)
+    CheckBox cbAtum;
+
+    @BindView(R.id.cbBacon)
+    CheckBox cbBacon;
+
+    @BindView(R.id.cbCalabresa)
+    CheckBox cbCalabresa;
+
+    @BindView(R.id.cbMussarela)
+    CheckBox cbMussarela;
+
+    @BindView(R.id.rgTamanhoPizza)
+    RadioGroup rgTamanhoPizza;
+
+    @BindView(R.id.spTipoPagamento)
+    Spinner spTipoPagamento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedido);
+
+        ButterKnife.bind(this);
+
+
     }
 }
