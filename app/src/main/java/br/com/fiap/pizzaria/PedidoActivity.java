9256e9bd1ead7b2417865f64e647f1af7fb18e89
jotace.assis <jotace.assis@outlook.com>
2017-10-07 11:47:43 -0300

    Criacao leiaute de loading

--- app/src/main/java/br/com/fiap/pizzaria/PedidoActivity.java
+++ app/src/main/java/br/com/fiap/pizzaria/PedidoActivity.java
@@ -7,23 +7,31 @@ import android.widget.CheckBox;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.fiap.pizzaria.model.Pedido;
 import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class PedidoActivity extends AppCompatActivity {
 
-    @BindView(R.id.cbAtum)
-    CheckBox cbAtum;
+//    @BindView(R.id.cbAtum)
+//    CheckBox cbAtum;
 
-    @BindView(R.id.cbBacon)
-    CheckBox cbBacon;
+//    @BindView(R.id.cbBacon)
+//    CheckBox cbBacon;
 
-    @BindView(R.id.cbCalabresa)
-    CheckBox cbCalabresa;
+//    @BindView(R.id.cbCalabresa)
+//    CheckBox cbCalabresa;
 
-    @BindView(R.id.cbMussarela)
-    CheckBox cbMussarela;
+//    @BindView(R.id.cbMussarela)
+//    CheckBox cbMussarela;
+
+    @BindViews( { R.id.cbAtum , R.id.cbBacon , R.id.cbCalabresa , R.id.cbMussarela} )
+    List<CheckBox> saboresPizza;
 
     @BindView(R.id.rgTamanhoPizza)
     RadioGroup rgTamanhoPizza;
@@ -41,6 +49,36 @@ public class PedidoActivity extends AppCompatActivity {
 
     @OnClick(R.id.btFecharPedido)
     public void fecharPedido() {
+
+        Pedido meuPedido = new Pedido();
+        meuPedido.setTipoPagamento(spTipoPagamento.getSelectedItem().toString());
+
+        List<String> sabores = new ArrayList<>();
+//        if (cbAtum.isChecked())
+//            sabores.add(cbAtum.getText().toString());
+//        if (cbBacon.isChecked())
+//            sabores.add(cbBacon.getText().toString());
+//        if (cbCalabresa.isChecked())
+//            sabores.add(cbCalabresa.getText().toString());
+//        if (cbMussarela.isChecked())
+//            sabores.add(cbMussarela.getText().toString());
+        for (CheckBox sabor : saboresPizza) {
+            if (sabor.isChecked())
+                sabores.add(sabor.getText().toString());
+        }
+        meuPedido.setSabor(sabores);
+
+        switch (rgTamanhoPizza.getCheckedRadioButtonId()) {
+            case R.id.rbTamanhoPequena:
+                meuPedido.setTamanho(getString(R.string.label_pequena));
+                break;
+            case R.id.rbTamanhoMedia:
+                meuPedido.setTamanho(getString(R.string.label_media));
+                break;
+            case R.id.rbTamanhoGrande:
+                meuPedido.setTamanho(getString(R.string.label_grande));
+                break;
+        }
 
     }
 }
